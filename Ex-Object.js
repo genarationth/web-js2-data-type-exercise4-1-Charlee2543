a853074c5d6b1b23968f8a096057e1c00e2a48bb
Charlee2543 <Charlee2543@hotmail.com>
2024-05-13 14:37:14 +0700

    Ex-ObjectinObject

---
+++ Ex-Object.js
@@ -0,0 +1,14 @@
+const Ex_products={
+    product_id:"9999",
+    product_name:"gaming",
+    quantity_in_stock:"99",
+    unit_price:"20,0000",
+    product_description:{
+        product_brand:"Lenovo",
+        size_or_quantity:"17 in",
+        taste_or_scent:"-",
+        colour:"blue",
+        additional_description:"like"
+    }
+};
+console.log(Ex_products);
